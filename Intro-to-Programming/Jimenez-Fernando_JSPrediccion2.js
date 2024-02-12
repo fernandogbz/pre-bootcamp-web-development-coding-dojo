5e2068bb53b85dc28b05cd9263928202dd378593
fernandogbz <fgjv06@gmail.com>
2024-02-12 18:53:41 -0300

    Fragmento de código 1

---
+++ Intro-to-Programming/Jimenez-Fernando_JSPrediccion2.js
@@ -0,0 +1,8 @@
+// Fragmento de código 1
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo); // ['Paula', 'Smith', 'Calle Principal 1234', 'St. Louis', 'MO', 12345]
+}
+muestraInformaciónContacto();
+
+
