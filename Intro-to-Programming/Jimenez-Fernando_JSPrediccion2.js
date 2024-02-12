c8ce5eb59e73209f06db88d2b9af6c470d07a36d
fernandogbz <fgjv06@gmail.com>
2024-02-12 19:02:12 -0300

    Fragmento de código 2

--- Intro-to-Programming/Jimenez-Fernando_JSPrediccion2.js
+++ Intro-to-Programming/Jimenez-Fernando_JSPrediccion2.js
@@ -5,4 +5,11 @@ function muestraInformaciónContacto() {
 }
 muestraInformaciónContacto();
 
-
+// Fragmento de código 2
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen); // ['brócoli', 'helado', 'croqueta de papa']
+}
+muestraListaDeCompras();
