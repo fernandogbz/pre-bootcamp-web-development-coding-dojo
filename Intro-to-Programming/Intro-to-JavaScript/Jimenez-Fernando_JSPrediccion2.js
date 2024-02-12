4cc15a4260ea0f4aeb912068dfca4fab9f130bf0
fernandogbz <fgjv06@gmail.com>
2024-02-12 19:43:02 -0300

    Intro to JavaScript

---
+++ Intro-to-Programming/Intro-to-JavaScript/Jimenez-Fernando_JSPrediccion2.js
@@ -0,0 +1,23 @@
+// Fragmento de código 1
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo); // ['Paula', 'Smith', 'Calle Principal 1234', 'St. Louis', 'MO', 12345]
+}
+muestraInformaciónContacto();
+
+// Fragmento de código 2
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen); // ['brócoli', 'helado', 'croqueta de papa']
+}
+muestraListaDeCompras();
+
+// Fragmento de código 3
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary); // ["Bambi", "Beetlejuice", "Toy Story", "Zoro"];
+
+
