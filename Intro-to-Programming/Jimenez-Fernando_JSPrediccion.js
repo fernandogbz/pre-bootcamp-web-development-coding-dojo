7256fe19057653b1008254780e87e18731be5a96
fernandogbz <fgjv06@gmail.com>
2024-02-12 17:29:15 -0300

    Predicción 1

---
+++ Intro-to-Programming/Jimenez-Fernando_JSPrediccion.js
@@ -0,0 +1,8 @@
+// Predicción 1: ¿Qué indicará console.log cuando se llame a esta función?
+
+function myBirthYearFunc(){
+        console.log("Nací en " + 1980); // Nací en 1980
+    }
+myBirthYearFunc()
+
+
