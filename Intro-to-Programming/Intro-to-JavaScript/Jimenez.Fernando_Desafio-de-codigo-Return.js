2d989e2eeb283fc6282ec99270db5f499da6284f
fernandogbz <fgjv06@gmail.com>
2024-02-12 21:18:08 -0300

    Fragmento de código 10

--- Intro-to-Programming/Intro-to-JavaScript/Jimenez.Fernando_Desafio-de-codigo-Return.js
+++ Intro-to-Programming/Intro-to-JavaScript/Jimenez.Fernando_Desafio-de-codigo-Return.js
@@ -77,3 +77,11 @@ var result = sumNums(2,3) + sumNums(3,5);
 console.log('result is', result); // result is 13
 
 
+//Fragmento de Código 10: un desafío arduo así que tómalo con calma
+function sumNums(num1, num2) {
+    var sum = num1 + num2;
+    console.log('sum is', sum); // sum is 5, sum is 3, sum is 6, sum is 3, sum is 5, sum is 8
+    return sum;
+}
+var result = sumNums(2,3) + sumNums(3,sumNums(2,1)) + sumNums(sumNums(2,1),sumNums(2,3));
+console.log('result is', result); // result is 19
